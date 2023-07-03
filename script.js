1a393da8d0338962eb2d3aa6a34dfc3d96952911
Adedoyinlorukomi <akindoyin800@gmail.com>
2023-07-03 18:25:59 +0100

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.write(5 - 2);
+document.getElementById("bedroom").innerHTML = "This text used to be a bedroom picture";
+
+function buttonColor(){
+    document.getElementById('button').style.backgroundColor = '#911';
+    document.getElementById('button').style.color = '#ffffff';
+}
